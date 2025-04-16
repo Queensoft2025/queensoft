f5cd28ceaf541ba00e210a4c5c30b7305810eda5
Queensoft2025 <queensoftsocial@gmail.com>
2025-04-17 00:39:44 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>👷‍♀️ Queensoft.in is Coming Soon!</h1>
+      <p>We're working hard to build something amazing. Stay tuned!</p>
+      <p>📧 Contact us: queensoftsocial@gmail.com <br/> 7550313614</p>
+
+    </div>
+  );
+}
+
+export default App;
