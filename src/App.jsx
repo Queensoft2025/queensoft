4404177db8c9d3cd73881030ed94f674b95d5b3b
Queensoft2025 <queensoftsocial@gmail.com>
2025-04-19 00:53:23 +0530

    Fixed logo path and updated homepage content

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import logo from "./assets/logo.png";
 
 function App() {
   return (
     <div className="container">
+      <img src={logo} alt="logo" />
       <h1>👷‍♀️ Queensoft.in is Coming Soon!</h1>
       <p>We're working hard to build something amazing. Stay tuned!</p>
       <p>📧 Contact us: queensoftsocial@gmail.com <br/> 7550313614</p>
